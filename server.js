a366dfce7c2b63dfb25ebdce0230e45ead63885e
Bima <abdulrahmanbima1@gmail.com>
2024-03-14 22:54:00 +0100

    Cors_config

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const eventRoute = require("./routes/eventRoute");
 const eventUserRoute = require("./routes/eventUserRoute");
 const orderRoute = require("./routes/orderRoute");
 const errorMiddlware = require("./middleware/errorMIddleWare");
-// const cors = require("cors");
+const cors = require("cors");
 
 const MONGO_URL = process.env.MONGO_URL;
 const PORT = process.env.PORT;
@@ -25,18 +25,15 @@ const corsOptions = {
 };
 
 const app = express();
-app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
-  res.header('Access-Control-Allow-Credentials', true);
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
-});
+
 
 app.use(cookieParser());
 
-// app.use(cors());
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
+
 
 app.use("/api/products", productRoute);
 app.use("/api/subscriber", subscriberRoute);
