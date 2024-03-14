4ee214f91d8ff0b2bfe7ac7b6ae1f3e9f98e3f0f
Bima <abdulrahmanbima1@gmail.com>
2024-03-14 22:23:56 +0100

    Added Access_control_middleware

--- server.js
+++ server.js
@@ -25,6 +25,13 @@ const corsOptions = {
 };
 
 const app = express();
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 app.use(cookieParser());
 
 app.use(cors());
